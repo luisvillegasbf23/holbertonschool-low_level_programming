d4bb5d6abf79adcf788e3fbb2fa6e622e87e6ef1
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-06 08:15:51 -0700

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts('"Programing is like building a multilingual puzzple');
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzple");
+	return (0);
+	}
