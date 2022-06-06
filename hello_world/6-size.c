ea9bae95fa64a75bf2cbd0e19efb8220bc7e2022
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-06 09:03:00 -0700

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(longType));
-	printf("Size of a long long int: %zu bytes(s)\n", sizeof(longlongType));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 	}
