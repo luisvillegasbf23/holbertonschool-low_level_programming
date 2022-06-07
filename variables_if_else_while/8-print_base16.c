ddb90b15feac7e1562fe9e4e9882bddbdf040350
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-07 08:34:57 -0700

    commit

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (num = 48; num <= 57; num++)
 		putchar(num);
-	for (num = 97; num <= 103; num++)
+	for (num = 97; num <= 102; num++)
 		putchar(num);
 	putchar(10);
 	return (0);
