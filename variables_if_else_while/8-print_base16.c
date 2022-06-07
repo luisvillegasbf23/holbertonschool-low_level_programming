6260b00f83fd917b82be32304391aee43a7ba1fb
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-07 08:33:28 -0700

    commit

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	for (num = 97; num <= 103; num++)
+		putchar(num);
+	putchar(10);
+	return (0);
+}
