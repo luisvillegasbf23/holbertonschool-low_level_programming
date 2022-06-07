2ebab4f10e857e599ede3acfa53bc2f56a252971
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-07 06:25:42 -0700

    commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	digit = n % 10;
 
-	if (n > 5 && n != 0)
+	if (n != 0 && n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	else if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, digit);
