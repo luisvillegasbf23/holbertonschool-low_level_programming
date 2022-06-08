4205b58da6e7eaed3c8dab4494b3ccbcdee6d1c1
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-08 06:12:50 -0700

    task 10

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,19 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = a + 49; b <= 57; b++ )
+		for (b = a + 1; b <= 9; b++)
 		{
-			putchar(a);
-			putchar(b);
-			if (a != 56)
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 8)
 			{
-				putchar(44);
-				putchar(32);
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
