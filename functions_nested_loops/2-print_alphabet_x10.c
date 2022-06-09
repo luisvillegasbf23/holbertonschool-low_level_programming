fe63710fc71c257f06be3c92d150f498d27aae9c
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-09 06:35:33 -0700

    commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * print_alphabet_x10 - this function print the alphabet in lowercase x10
  *
  * Return: Always 0
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 {
 	char i;
 	char j;
+
 	for (i = 1 ; i <= 10; i++)
 	{
 		for (j = 97; i <= 122; j++)
