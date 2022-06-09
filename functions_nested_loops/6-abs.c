0c1c4f113289168dbd9c2cef5d6ec861788dad72
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-09 08:38:55 -0700

    commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - This function computes the absolute value of an integer.
+ * @x: The caracter to print
+ * Return: Always 0
+ */
+int _abs(int x)
+{
+	return (x);
+}
