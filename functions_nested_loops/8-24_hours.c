1bee76e132caaab8fb436cb4c0eb77524572c080
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-09 10:39:54 -0700

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,3 +1,25 @@
 #include "main.h"
 /**
- * jack_bauer
+ * jack_bauer - This function display every minute of the day of Jack Bauer
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hrs;
+	int mint;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mint = 0; mint < 60; mint++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mint / 10) + '0');
+			_putchar((mint % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
