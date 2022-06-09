9891b624a16c764e523db31580cc646c9e2e99ac
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-09 07:19:46 -0700

    commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - this function check if it is lowercase
+ * @c: the caracter to print
+ * Return: ALways 0
+ */
+int _islower(int c)
+{
+	char i;
+
+	if (islower(i))
+		_putchar(48);
+	else
+		_putchar(49);
+	return (0);
+}
