5e2799addb70a2712a77b57a781a95c13d1432d4
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-10 06:10:21 -0700

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - This function multiplies two values
+ * @a: The character to print one
+ * @b: The character to print two
+ * Return: r
+ */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
