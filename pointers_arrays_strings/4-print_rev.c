bbfbbbdab1bf59655cf6bbec0df3e0f907982be0
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-14 10:42:58 -0700

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev -  function that prints a string, in reverse,
+ * followed by a new line
+ * @s: Pointer
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	j = _strlen(s);
+
+	for (i = j; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar(10);
+}
