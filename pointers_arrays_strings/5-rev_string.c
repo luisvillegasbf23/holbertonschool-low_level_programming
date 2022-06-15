58fa1854470de3e9d97675f94a38688a54e34fbf
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-15 09:19:20 -0700

    commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,13 @@
  */
 void rev_string(char *s)
 {
-	int str, i;
+	int str, i, temp;
 
 	for (str = 0 ; s[str]; str++) /** longitud del string */
 		;
 	for (i = 0; i < str / 2; i++)
 	{
-		char temp = s[i];
-
+		temp = s[i];
 		s[i] = s[str - i - 1];
 		s[str - i - 1] = temp;
 	}
