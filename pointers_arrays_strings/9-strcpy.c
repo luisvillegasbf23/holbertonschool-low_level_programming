6c8009055ccf7570c73f81388e1c0d71ee866a97
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-16 05:33:18 -0700

    commit

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *_strcpy - Write a function that copies the string pointed to by src,
+ * @dest: Pointer
+ * @src: Pointer
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	return (dest);
+}
