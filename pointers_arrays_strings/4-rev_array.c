f2498f264da5cd57a40349c0b80f36bb7002f594
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-16 09:40:14 -0700

    commit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - Write a function that reverses the 
+ * content of an array of integers.
+ * @a: pointer
+ * n: parametre
+ * Return: Always0
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[n]; i++)
+		;
+	for(i -= 1; i >= 0; i--)
+		;
+}
