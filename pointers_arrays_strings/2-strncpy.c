75e3c81563618242707052e26eb11b7ddaaadf15
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-16 07:46:21 -0700

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,13 +4,14 @@
  * @dest: Pointer
  * @src: Pointer
  * @n: Number
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && dest[i]; i++)
-		dest[i]= src [i];
+	for (i = 0; i < n && src[i]; i++)
+		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
