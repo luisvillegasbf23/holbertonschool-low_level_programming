c95590e9b6b39dc995ca8a12449c457e2d46fcc4
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-16 09:18:03 -0700

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] == s2[i]; i++)
-		return (0);
-	return ((s1[i] > s2[i]) - (s1[i] < s2[i]));
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+	if (s1[i] < s2[i])
+		return (s1[i] - s2[i]);
+	if (s1[i] > s2[i])
+		return (s1[i] - s2[i]);
+	return (0);
 }
