88dc901edeab3d27df388be53e1fadc804d8ea2b
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-16 11:37:12 -0700

    commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *cap_string - Write a function that capitalizes all words of a string.
+ * @s: Pointer
+ * Return: Always 0
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	for (; s[i]; i++)
+	{
+		if (s[i] == 9 || s[i] == 10 || s[i] == 32 || s[i] == 44
+						|| s[i] == 59 || s[i] == 46 || s[i] == 40
+						|| s[i] == 41 || s[i] == 34 || s[i] == 63
+						|| s[i] == 33 || s[i] == 123 || s[i] == 125)
+		{
+				if (s[i + 1] > 96 && s[i + 1] < 123)
+					s[i + 1] = s[i + 1] - 32;
+		}
+	}
+	return (s);
+}
