a6a42579b60a212073ada85e258740a16e2c2487
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-19 10:28:55 -0700

    commit

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -18,9 +18,9 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (s[i] == accept[j])
 				return (&s[i]);
-			else
-				return (NULL);
 		}
 	}
+	if (s[i] != accept[j])
+		return (NULL);
 	return (&s[i]);
 }
