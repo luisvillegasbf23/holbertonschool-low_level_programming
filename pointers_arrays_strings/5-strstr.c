8845e26bac60dbcfe1e65b4eb5b0fd3537fb6b65
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-19 14:20:23 -0700

    commit

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j]; j++)
 		{
-			if(needle[j] != haystack[i + j])
+			if (needle[j] != haystack[i + j])
 				break;
 		}
 		if (needle[j] == '\0')
