bf1d061b6cf3b6cfeb02c64adaabd9cade68841d
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-20 12:49:22 -0300

    commit

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion -Write a function that prints a string in reverse
+ * @s: Pointer
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar (10);
+}
