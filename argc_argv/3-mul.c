5bd0a3165e9f2fec5dd26a88dd51c93662acb16a
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-21 10:18:14 -0700

    commit

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main fuction
+ * @argc: saves the memory address
+ * @argv: pointer to pointer
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int res;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
