bd2e1d1c1ed4c9c252a06944977a8d18b6f37404
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-21 08:29:38 -0700

    commit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - 
+ * main - main fuction
  * @argc: saves the memory address
  * @argv: pointer to pointer
  * Return: Always 0
  */
 
-int main( int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
