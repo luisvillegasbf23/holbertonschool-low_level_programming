306b7154d0907886cf573f999270139d6b1d217d
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-23 14:00:49 -0300

    commit

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,11 +19,11 @@ char *str_concat(char *s1, char *s2)
 	concat = malloc(a + b + 1);
 	if (concat == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < a; i++)
 		concat[i] = s1[i];
-	for (j = 0; s2[j]; j++)
+	for (j = 0; i < b; j++)
 	{
-		concat[i +j] = s2[j];
+		concat[i + j] = s2[j];
 	}
 	concat[i + j] = '\0';
 	return (concat);
