f65c7dc064a2d299118fb6e34162f6e098cd011c
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-23 12:54:26 -0300

    commit

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	copy = malloc(strlen(str));
 	if (copy == NULL)
 		return (NULL);
-	for (i = 0; str[i] == NULL; i++)
+	for (i = 0; str[i] == '\0'; i++)
 		copy[i] = str[i];
 	return (copy);
 }
