ac58bcca4723902eb45d530a2c7d050a41150378
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-23 16:09:36 -0300

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,6 +14,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 		grid[i] = malloc(width * sizeof(int));
 	return (grid);
