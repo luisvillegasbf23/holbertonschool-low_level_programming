0faa0097192dd698bf45b5c9945b834ffc72eff2
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-23 16:05:51 -0300

    commit

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * **alloc_grid - Write a function that returns a pointer to
+ * a 2 dimensional array of integers.
+ * @width: int
+ * @height: int
+ * Return: grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int **grid = NULL;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+		grid[i] = malloc(width * sizeof(int));
+	return (grid);
+}
