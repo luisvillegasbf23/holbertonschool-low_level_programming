1693d148bc894b2b4665d9328c037c0b14193ce6
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-24 01:31:58 -0300

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,22 +8,34 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, size;
+	int i, j;
 	int **grid = NULL;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
-	size = width * height;
-	grid = malloc(size * sizeof(int));
-
-	if (grid != NULL)
 	{
-		for (i = 0; i < height; i++)
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
 		{
-			for (j = 0; j < width; j++)
-				grid[i][j] =0;
+			free(grid);
+			for (j = 0; j <= i; j++)
+			{
+				free(grid[j]);
+			}
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
 		}
 	}
-
 	return (grid);
 }
