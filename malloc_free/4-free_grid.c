36d5602509ced1c9905f6c0a08ef6b88de69d3d5
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-24 01:42:09 -0300

    commit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * free_grid - returns a pointer to a 2 dimensional array of integers
+ * @grid: arrays bidimensional
+ * @height: height
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
