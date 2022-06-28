8076fcbb17707395453ddc2e8811146ac25d077f
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-28 12:56:20 -0300

    commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("(nill)\n");
+		printf("(nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
