44d986b3119b7211854488ed6538bb441d019372
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-28 08:00:39 -0700

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,16 +1,52 @@
 #include "main.h"
 /**
- * print_times_table  - prints the table
- * @n: number to print
+ * times_table - prints the 9 times table
+ * @n: character to print
  * Return: 1 sucess or 0 in fail
  */
 void print_times_table(int n)
 {
-	int x, y, res;
-	if (n < 0 || n > 15)
-	{
-		_putchar(10);
+	int r, a, b;
+
+	if (n > 15 || n < 0)
 		return;
+	
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{	
+			r = a * b;
+			if (b == 0)
+				_putchar(r + '0');
+
+			if ( r <= 9)
+			{	
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+
+			if ( r <= 99 && r > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+
+			if (r > 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((r / 100) + '0');
+				_putchar(((r / 10) % 10) + '0');
+				_putchar((r % 10) + '0');
+
+			}
+		}
+		_putchar(10);
 	}
-	for 
 }
