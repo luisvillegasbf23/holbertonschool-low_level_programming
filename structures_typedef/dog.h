49cb883fdc3570ef400910e7f66fce125b7c4817
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-28 13:06:09 -0300

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,7 +17,19 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**
+ * struct dog_t - dog :)
+ * @name: string
+ * @age: float
+ * @owner: string
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+int main(void);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
