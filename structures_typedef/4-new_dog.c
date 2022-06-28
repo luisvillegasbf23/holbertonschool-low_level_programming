1d99191ddc2967f213a4279bc23dc5a1c2cb189e
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-28 15:22:49 -0300

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,10 +9,31 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *p = malloc (sizeof(name) + sizeof(age) + sizeof(owner));
+	dog_t *p;
 
-	p->name = name;
+	p = malloc(sizeof(dog_t));
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->name = malloc(strlen(name) + 1);
+	if (p->name == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	p->owner = malloc(strlen(owner) + 1);
+	if (p->owner == NULL)
+	{
+		free(p->name);
+		free(p);
+		return (NULL);
+	}
+
+	strcpy(p->name, name);
+	strcpy(p->owner, owner);
 	p->age = age;
-	p->owner = owner;
 	return (p);
 }
