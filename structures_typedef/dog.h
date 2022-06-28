e19e078db164bfc720c7ff0b1d535dc5a5c8ca23
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-28 11:40:18 -0300

    commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct gog - Dog type
+ * @name: Firts member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: this structure is to registering dogs
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
