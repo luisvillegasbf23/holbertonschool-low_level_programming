8379ab79e60714be4e8c4d81930d2e02eb08fd45
luisvillegasbf23 <luisville.lv@gmail.com>
2022-06-29 06:38:17 -0700

    commit

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Write a function that prints a name
+ * @name: Pointer
+ * @f: function pointer
+ * return: always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		(*f)(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+This proyect is abour function pointers
