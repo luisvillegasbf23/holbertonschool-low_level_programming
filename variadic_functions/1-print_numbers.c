3df8a9e8a703fd5fa0bfae1e9e518c155d473c13
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-01 13:05:36 -0300

    commit

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,16 +11,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, num;
 	va_list par;
 
-	if  (separator == NULL)
-		return;
-	va_start(par, n);
-	for (i = 0; i < n; i++)
+	if  (separator != NULL)
 	{
-		num = va_arg(par, int);
-		printf("%d", num);
-		if (i < n - 1)
-			printf("%s", separator);
+		va_start(par, n);
+		for (i = 0; i < n; i++)
+		{
+			num = va_arg(par, int);
+			printf("%d", num);
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(par);
 	}
-	printf("\n");
-	va_end(par);
 }
