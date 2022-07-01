a98641e638758bbe4b1d043091d1474f5ce98426
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-01 14:44:39 -0300

    commit

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,21 +9,22 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *s;
 	va_list par;
 
 	va_start(par, n);
 	for (i = 0; i < n; i++)
 	{
+		s = va_arg(par, char *);
+
 		if (separator != NULL)
 		{
-			if (!va_arg(par, char *))
-				printf("(nill)");
-			printf("%s", va_arg(par, char *));
+			printf("%s", s);
 			if (i < n -1)
 				printf("%s", separator);
 		}
 		else
-			printf("%s", va_arg(par, char *));
+			printf("%s", s);
 
 	}
 	printf("\n");
