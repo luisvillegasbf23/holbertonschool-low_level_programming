931038bc2b5546261ddbdc40268300181c6cbfd4
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-01 16:02:22 -0300

    commit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+* print_all - function that prints anything
+* @format: if 'c, s, i or f' passed, acts like a printf() functions
+* otherwise, ignores the character and prints the args passed
+* Return: Void
+*/
+void print_all(const char * const format, ...)
+{
+	unsigned i;
+	va_list par;
+
+	i= 0;
+	va_start(par, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+					printf("%c", va_arg(par, int));
+					printf(", ");
+					break;
+			case 'i':
+					printf("%d", va_arg(par, int));
+					break;
+			case 'f':
+					printf("%f", va_arg(par, double));
+					break;
+			case 's':
+					printf("%s", va_arg(par, char *));
+					break;
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(par);
+}
