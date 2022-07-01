ef2cebf399d6de84f3bbd06a11d2052cc3c5e179
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-01 14:17:48 -0300

    commit

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Write a function that prints strings,
+ * followed by a new line. 
+ * @separator: pointer
+ * @n: number of variables
+ * Return: Void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list par;
+
+	va_start(par, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+		{
+			if (!i)
+				printf("(nill)");
+			else
+				printf("%s", va_arg(par, char *));
+			if (i < n -1)
+				printf("%s", separator);
+		}
+		else
+			printf("%s", va_arg(par, char *));
+
+	}
+	printf("\n");
+	va_end(par);
+}
