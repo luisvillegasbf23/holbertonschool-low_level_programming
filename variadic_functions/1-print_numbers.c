be2796ee3672d0c5e609f1b9af6d68016e869025
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-01 13:00:54 -0300

    commit

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Write a function that prints numbers,
+ * followed by a new line.
+ * @separator: pointer
+ * @n: number of variables
+ * Return: Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, num;
+	va_list par;
+
+	if  (separator == NULL)
+		return;
+	va_start(par, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(par, int);
+		printf("%d", num);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(par);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
+
