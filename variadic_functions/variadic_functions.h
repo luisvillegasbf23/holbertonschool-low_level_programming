41285d96c982cca7bc499a82be2b14beda9679cf
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-01 13:03:50 -0300

    commit

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
+#include <stddef.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
