201355570c8df9e182bce6212af6e03e644922f1
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-13 13:11:15 -0300

    task 2

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,4 +22,5 @@ struct list_s *next;
 /* prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
