a397b41993de515398a13de3d35e207fca721c33
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-13 15:27:38 -0300

    task 0 fix betty documentation styles

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_list - Write a function that prints all the
+ * print_listint - Write a function that prints all the
  * elements of a listint_t list.
  * @h: pointer to head
  * Return: the number of nodes
