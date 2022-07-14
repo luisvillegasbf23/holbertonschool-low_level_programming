c4c6bb57a0e8ec192106b74ebcddd13eb72b5cf6
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-14 14:39:38 -0300

    fix task 9wq

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *auxp = *head;
+	listint_t *temp = *head;
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
@@ -18,18 +18,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	if (idx == 0)
-	{	new->next = auxp;
+	{	new->next = temp;
 		*head = new;
 		return (new);
 	}
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (!auxp)
+		if (!temp)
 			return (NULL);
-		auxp = auxp->next;
+		temp = temp->next;
 	}
 
-	new->next = auxp->next;
-	auxp->next = new;
+	new->next = temp->next;
+	temp->next = new;
 	return (new);
 }
