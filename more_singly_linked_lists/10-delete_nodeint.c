cc6044d218fbc9a8b7f966be27bef18b4abded81
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-14 14:49:11 -0300

    fix betty coding styles task 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index - Write a function that deletes the 
+ * delete_nodeint_at_index - Write a function that deletes the
  * node at index index of a listint_t linked list.
  * @head: node head
  * @index: index
