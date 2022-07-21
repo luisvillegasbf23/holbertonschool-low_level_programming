17d53ba940cecacac906c551fe53b98530f6dd37
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-21 13:08:43 -0300

    fix task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,12 +15,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node)
 	{
 		new_node->n = n;
-		new_node->next = *head;
 		new_node->prev = NULL;
+		new_node->next = NULL;
+		new_node->next = *head;
+		if (*head != NULL)
+			(*head)->prev = new_node;
 		*head = new_node;
 	}
-	else if (*head != NULL)
-		(*head)->prev = new_node;
 	else
 		free(new_node);
 	return (new_node);
