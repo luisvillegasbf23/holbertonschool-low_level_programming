38ea0e1e42367f9620eb8214832cd1de1b535f74
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-21 12:08:26 -0300

    commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,8 +18,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->next = (*head);
 		new_node->prev = NULL;
 		(*head) = new_node;
-		if ((*head) != NULL)
-			(*head)->prev = new_node;
 	}
 	else
 		free(new_node);
