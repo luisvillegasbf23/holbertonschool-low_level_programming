c752731fa277160c1ec9bb42b7b82ce1e1d5a299
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-21 14:07:13 -0300

    fix Task 3 (betty codding styles)

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,7 +2,8 @@
 /**
  * *add_dnodeint_end - Write a function that adds a new node
  * at the end of a dlistint_t list.
- * @head: pointer to pointer head node 
+ * @head: pointer to pointer head node
+ * @n: data
  * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
