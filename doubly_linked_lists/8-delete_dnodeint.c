ee0e46db708002540b1f0a78fd734c43b402a342
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-22 14:35:18 -0300

    fix task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,38 +10,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *delete = *head;
-	dlistint_t *temp;
 
 	if (*head)
 	{
-		temp = *head;
-		/* if index is 1 make head next as head and delete prev head*/
 		if (index == 0)
 		{
 			*head = (*head)->next;
 			free(delete);
-			if (*head)
-				(*head)->prev = NULL;
 			return (1);
 		}
-		/* else, make a temp node and traverse to the node prev*/
-		else
+		for (i = 0; i < index && delete; i++)
+			delete = delete->next;
+		if (i == index)
 		{
-			for (i = 0; i < index - 1 && (*head)->next; i++)
-				temp = temp->next;
-		}
-		/**
-		 * If the previous node and next of the previous
-		 * is not null, adjust links
-		 */
-		if (temp && temp->next)
-		{
-			delete = temp->next;
-			temp->next = temp->next->next;
-			if (temp->next->next)
-				temp->next->next->prev = temp->next;
+			if (delete->prev)
+				delete->prev->next = delete->next;
+			if (delete->next)
+				delete->next->prev = delete->prev;
 			free(delete);
-			return (1);
+			return(1);
 		}
 	}
 	return (-1);
