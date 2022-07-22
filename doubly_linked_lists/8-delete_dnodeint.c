2d36fff5d707e7b2a0af48ab95b794d9aa67d557
luisvillegasbf23 <luisville.lv@gmail.com>
2022-07-22 13:35:24 -0300

    fix task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -41,6 +41,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (temp->next->next)
 				temp->next->next->prev = temp->next;
 			free(delete);
+			return (1);
 		}
 	}
 	return (-1);
